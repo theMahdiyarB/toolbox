075900e8ad339b4959567f43fa27ad17fcfe7617
Mahdiyar Baladzadeh <m.baladzadeh@gmail.com>
2026-04-14 00:03:45 +0330

    Fixed some bugs in online tools Added some resources to iran.html

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 //    - External/opaque requests: Network-only (skip)
 // ============================================================
 
-const CACHE_VERSION = 'toolbox-v4.0';
+const CACHE_VERSION = 'toolbox-v4.2';
 const CACHE_SHELL   = CACHE_VERSION + '-shell';
 const CACHE_ASSETS  = CACHE_VERSION + '-assets';
 
